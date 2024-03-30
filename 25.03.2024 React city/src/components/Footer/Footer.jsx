28b9ab37220efa2bb53521c6d7dd7651c8d7b03c
ArtemB <artem.buldakov.98@mail.ru>
2024-03-30 19:41:18 +0300

    Дз - создание страницы с информацией о своем городе

---
+++ 25.03.2024 React city/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Footer.css';
+
+
+function Footer(){
+    return(
+        <div className="FooterContainer">
+            <div className="FooterInfo">
+                <div>https://город-россии.рф/</div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ 25.03.2024 React city/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './Header.css';
+
+
+
+function Header () {
+    return (
+        <div className="Container">
+            <div className="Header_container">
+                <div className="Header_logo_container"></div>
+                <div className="Header_info">
+                    <p className="City_info">Ижевск</p>
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default Header;
